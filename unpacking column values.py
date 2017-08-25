dad3c50e2911830ad1cc9fbcfb30c06e34d80b46
chidamodu <chidambaram.spv@gmail.com>
2017-08-25 07:17:07 -0700

    Update unpacking column values.py

--- unpacking column values.py
+++ unpacking column values.py
@@ -13,13 +13,13 @@ I am interested in splitting, say, each of 10 values in row 2 under the column '
 
 df1=(df1.set_index(df1.columns.drop('Description',1).tolist()).Description.str.split(';', expand=True).stack().reset_index().rename(columns={0:'Description'}).loc[:, df1.columns])
 #Breaking the above code down in parts:
-#df.columns.drop('Description',1).tolist(): It removes 'Description' in axis=1 and returns the labels['Resolution Letter', 'Action', 'Type'] in list type.
+#df1.columns.drop('Description',1).tolist(): It removes 'Description' in axis=1 and returns the labels['Resolution Letter', 'Action', 'Type'] in list type.
 #Description.str.split(';', expand=True).stack():It splits the values in Description at ';' and palces each value in a column. Here 'expand=True' extends the number of columns based on the maximum number of values, say, after splitting if there are 10 values in one row and 20 values in another. It extends the number of columns to 20. The rows that do not have that many values will have 'none'. The 'stack()' option puts these values in rows. Why 'stack()'? because I want each value in a separate row.
 #reset_index(): This option will reset the index from 0. Prior to this step the dataset retains the two types of indices: one from before splitting the 'Description' column and one from after splitting.
-#rename(columns={0:'Description'}).loc[:, df1.columns]: sets the space for other columns based on the number of row values of Description.
-#Chaining the above methods and setting index according to the new change by using set_index.
+#rename(columns={0:'Description'}).loc[:, df1.columns]: Sets the space for other columns based on the number of row values of Description.
+#df1.set_index: Chaining the above methods and setting index according to the new change by using set_index.
 
-"""As expected the output looks like (after the change):
+"""After the change, the same row1, with values of 'Description' unpacked, looks like:
 
 		Resolution Letter	Action					Type								Description
 	
