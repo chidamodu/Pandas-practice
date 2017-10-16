3368f4b6067cdee184a57d2e3e03d140e16ff9b9
Chidam Raji <chidambaram.spv@gmail.com>
2017-10-15 18:46:59 -0700

    Groupby + Transform
    Transform will look at the dataframe columns one by one and return back a series (or group of series) 'made' of scalars. It's useful in assigning results of a reduction function back to original dataframe!

--- groupby with transform.py
+++ groupby with transform.py
@@ -1,1 +1,1 @@
-df['new_column_name'] = df.groupby('cn')['cn'].transform('count') #Groupby column 'cn' and the second 'cn' is what I count on. It returns the dataset with a new column added with count values in the name: 'new_column_name'
+df['new_column_name'] = df.groupby('cn')['cn'].transform('count')#Groupby column 'cn' and the second 'cn' is what I count on using transform. Transform returns the count values which will be broadcasted to all the rows in the group. The result is then stored in a new column by name: 'new_column_name'
